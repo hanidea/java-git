0d182aeff13d6170cee14df1dee37f15fae05948
hanidea <hydavid@126.com>
2019-04-22 10:55:48 +0800

    Update main.dart
    container

--- flutterfirst/lib/main.dart
+++ flutterfirst/lib/main.dart
@@ -12,7 +12,26 @@ class MyApp extends StatelessWidget{
           title:Text('Hello World'),
         ),
         body:Center(
-          child:Text('Hello World James')
+          // child:Text('Hello World James Hello World JamesHello World JamesHello World JamesHello World JamesHello World James',
+          // textAlign: TextAlign.left,
+          // style: TextStyle(
+          //   fontSize: 25.0,
+          //   color:Color.fromARGB(255, 255, 150, 150),
+          //   decoration: TextDecoration.underline,
+          //   decorationStyle: TextDecorationStyle.solid
+          // ),
+          // // maxLines: 2,
+          // // overflow: TextOverflow.fade,
+          // )
+          child: Container(
+            child: new Text('Hello James',
+            style:TextStyle(fontSize: 40.0),
+            ),
+            alignment: Alignment.center,
+            width: 500.0,
+            height: 400.0,
+            color:Colors.lightBlue
+          ),
         )
       )
     );
