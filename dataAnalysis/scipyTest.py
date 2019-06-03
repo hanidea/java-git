d32ff8bde7b1bbf85d0b6755df27fc677d6e2899
hanidea <hydavid@126.com>
2019-06-03 21:06:38 +0800

    scipytest
    demo

--- dataAnalysis/scipyTest.py
+++ dataAnalysis/scipyTest.py
@@ -1,6 +1,6 @@
 #encoding=utf-8
 import numpy as np
-import scipy.optimize as opt
+from pylab import *
 def main():
     #Integral
     from scipy.integrate import quad,dblquad,nquad
@@ -21,14 +21,46 @@ def main():
     res=minimize(rosen,x0,method="nelder-mead",options={"xtol":1e-8,"disp":True})
     print("ROSE MINI:",res.x)
     def func(x):
-        return(2*x[0]*x[1]+2*x[0]-x[0]**2-2*x[1]**2)
+        return -(2*x[0]*x[1]+2*x[0]-x[0]**2-2*x[1]**2)
     def func_deriv(x):
-        dfdx0 = (-2*x[0]+2*x[1]+2)
-        dfdx1 = (2*x[0]-4*x[1])
-        return np.array(dfdx0,dfdx1)
+        dfdx0 = -(-2*x[0]+2*x[1]+2)
+        dfdx1 = -(2*x[0]-4*x[1])
+        return np.array([dfdx0,dfdx1])
     cons=({"type":"eq","fun":lambda x:np.array([x[0]**3-x[1]]),"jac":lambda x:np.array([3.0*(x[0]**2.0),-1.0])},
           {'type':'ineq','fun':lambda x:np.array([x[1]-1]),'jac':lambda x:np.array([0.0,1.0])})
     res = minimize(func,[-1.0,1.0],jac=func_deriv,constraints=cons,method='SLSQP',options={'disp':True})
     print("RESTRICT:",res)
+    from scipy.optimize import root
+    def fun(x):
+        return x+2*np.cos(x)
+    sol=root(fun,0.1)
+    print("ROOT:",sol.x,sol.fun)
+    #Interpolation
+    x=np.linspace(0,1,10)
+    y=np.sin(2*np.pi*x)
+    from scipy.interpolate import interp1d
+    li=interp1d(x,y,kind="cubic")
+    x_new=np.linspace(0,1,50)
+    y_new= li(x_new)
+    figure()
+    plot(x,y,"r")
+    plot(x_new,y_new,"k")
+    show()
+    print(y_new)
+    #Linear
+    from scipy import linalg as lg
+    arr=np.array([[1,2],[3,4]])
+    print("Det:",lg.det(arr))
+    print("Inv:",lg.inv(arr))
+    b=np.array([6,14])
+    print("Sol:",lg.solve(arr,b))
+    print("Eig:",lg.eig(arr))
+    print("LU:",lg.lu(arr))
+    print("QR:",lg.qr(arr))
+    print("SVD:",lg.svd(arr))
+    print("Schur:",lg.schur(arr))
+
+
+
 if __name__ == "__main__":
     main()
