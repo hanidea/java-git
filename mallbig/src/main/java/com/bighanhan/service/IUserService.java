4a27e691ac212d4af4a2178cedf8f03d86d3f844
hanidea <hydavid@126.com>
2019-08-04 14:35:47 +0800

    md5

--- mallbig/src/main/java/com/bighanhan/service/IUserService.java
+++ mallbig/src/main/java/com/bighanhan/service/IUserService.java
@@ -5,4 +5,6 @@ import com.bighanhan.pojo.User;
 
 public interface IUserService {
     ServerResponse<User> login(String username, String password);
+    ServerResponse<String> register(User user);
+    ServerResponse<String> checkValid(String str,String type);
 }
