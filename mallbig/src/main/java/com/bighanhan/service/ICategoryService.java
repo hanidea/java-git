2e8655b5868836405b1fd28caa2269a3bc7dd70a
hanidea <hydavid@126.com>
2019-08-09 13:58:04 +0800

    category

--- mallbig/src/main/java/com/bighanhan/service/ICategoryService.java
+++ mallbig/src/main/java/com/bighanhan/service/ICategoryService.java
@@ -1,8 +1,13 @@
 package com.bighanhan.service;
 
         import com.bighanhan.common.ServerResponse;
+        import com.bighanhan.pojo.Category;
+
+        import java.util.List;
 
 public interface ICategoryService {
     ServerResponse addCategory(String categoryName, Integer parentId);
     ServerResponse updateCategoryName(Integer categoryId,String categoryName);
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+    ServerResponse<List<Integer>> selectCategoryAndChildrenById(Integer categoryId);
 }
