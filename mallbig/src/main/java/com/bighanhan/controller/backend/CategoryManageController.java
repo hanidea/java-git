22fca169899b9f529167fc20a915a1bac230f05b
hanidea <hydavid@126.com>
2019-08-09 11:19:10 +0800

    ICategory

--- mallbig/src/main/java/com/bighanhan/controller/backend/CategoryManageController.java
+++ mallbig/src/main/java/com/bighanhan/controller/backend/CategoryManageController.java
@@ -4,11 +4,13 @@ import com.bighanhan.common.Const;
 import com.bighanhan.common.ResponseCode;
 import com.bighanhan.common.ServerResponse;
 import com.bighanhan.pojo.User;
+import com.bighanhan.service.ICategoryService;
 import com.bighanhan.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
@@ -19,6 +21,11 @@ public class CategoryManageController {
     @Autowired
     private IUserService iUserService;
 
+    @Autowired
+    private ICategoryService iCategoryService;
+
+    @RequestMapping("add_category.do")
+    @ResponseBody
     public ServerResponse addCategory(HttpSession session,String categoryName, @RequestParam(value = "parentId",defaultValue = "0")int parentId){
         User user = (User)session.getAttribute(Const.CURRENT_USER);
         if(user == null){
@@ -30,6 +37,21 @@ public class CategoryManageController {
             //增加我们处理分类的逻辑
             return iCategoryService.addCategory(categoryName,parentId);
 
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作,需要管理员权限");
+        }
+    }
+
+    @RequestMapping("set_category_name.do")
+    @ResponseBody
+    public ServerResponse setCategoryName(HttpSession session,Integer categoryId,String categoryName){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录");
+        }
+        if(iUserService.checkAdminRole(user).isSuccess()){
+            //更新categoryName
+            return iCategoryService.updateCategoryName(categoryId,categoryName);
         }else{
             return ServerResponse.createByErrorMessage("无权限操作,需要管理员权限");
         }
--- mallbig/src/main/java/com/bighanhan/service/ICategoryService.java
+++ mallbig/src/main/java/com/bighanhan/service/ICategoryService.java
@@ -1,4 +1,8 @@
 package com.bighanhan.service;
 
+        import com.bighanhan.common.ServerResponse;
+
 public interface ICategoryService {
+    ServerResponse addCategory(String categoryName, Integer parentId);
+    ServerResponse updateCategoryName(Integer categoryId,String categoryName);
 }
