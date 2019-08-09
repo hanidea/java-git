bc171fce04d6782a6ce2f0e9bcccaadca171a159
hanidea <hydavid@126.com>
2019-08-09 10:57:01 +0800

    category

---
+++ mallbig/src/main/java/com/bighanhan/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,8 @@
+package com.bighanhan.service.impl;
+
+import com.bighanhan.service.ICategoryService;
+import org.springframework.stereotype.Service;
+
+@Service("iCategoryService")
+public class CategoryServiceImpl implements ICategoryService {
+}
