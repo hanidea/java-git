42387bd56c8225746035d757060afaddeb287209
hanidea <hydavid@126.com>
2022-01-19 11:02:41 +0800

    lol

---
+++ lol/src/Main.java
@@ -0,0 +1,14 @@
+import test.A;
+import test.B;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args){
+//        A a = new A();
+//        a.print();
+        //System.out.println("Hello World!");
+        B b = new B();
+        b.print();
+    }
+}
---
+++ lol/src/test/A.java
@@ -0,0 +1,9 @@
+package test;
+
+public class A {
+    private C c;
+    public void print(){
+        C c = new C();
+        System.out.println("This is Class A");
+    }
+}
