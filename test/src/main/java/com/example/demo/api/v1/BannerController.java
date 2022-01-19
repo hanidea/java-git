c2e42fc7deeee7944f68722f6c9767001b7b946b
hanidea <hydavid@126.com>
2022-01-19 16:34:00 +0800

    banner

--- test/src/main/java/com/example/demo/api/v1/BannerController.java
+++ test/src/main/java/com/example/demo/api/v1/BannerController.java
@@ -1,5 +1,8 @@
 package com.example.demo.api.v1;
 
+import com.example.demo.Service.BannerService;
+import com.example.demo.sample.hero.Diana;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,9 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 @RequestMapping("/v1/banner")
 public class BannerController {
+    @Autowired
+    private Diana diana;
     //RestFul API
+    @Autowired
+    private BannerService bannerservice;
     @GetMapping("/test")
     public String test(){
+        diana.r();
         return "Hello,James";
     }
 }
