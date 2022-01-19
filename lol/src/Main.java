ddf6bc4212a44cbc1a51ad092a0fc205f52e82af
hanidea <hydavid@126.com>
2022-01-19 11:14:36 +0800

    test

--- lol/src/Main.java
+++ lol/src/Main.java
@@ -1,14 +1,22 @@
+import awkward.hero.Diana;
 import test.A;
 import test.B;
 
 import java.awt.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-//        A a = new A();
-//        a.print();
-        //System.out.println("Hello World!");
-        B b = new B();
-        b.print();
+        String name = Main.getPlayerInput();
+        if(name.equals("Diana")){
+            Diana diana = new Diana();
+            diana.r();
+        }
+    }
+    private static String getPlayerInput(){
+        System.out.println("Enter a Hero's Name");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+
     }
 }
