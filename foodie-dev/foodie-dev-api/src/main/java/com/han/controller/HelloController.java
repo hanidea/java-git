eeb2a089f0ad263cd6497f631788daee2570dc4b
hanidea <hydavid@126.com>
2022-12-07 15:30:21 +0800

    foodie

---
+++ foodie-dev/foodie-dev-api/src/main/java/com/han/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.han.controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public Object hello(){
+        return "Hello World~";
+    }
+}
